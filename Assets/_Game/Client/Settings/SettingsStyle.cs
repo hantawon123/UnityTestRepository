cf8bccae0a57b1d5ca0cdc6a02d326312736c070
yanghl0527 <yanghl0527@naver.com>
2026-09-10 16:35:39 +0900

    S15P21D205-921 [CL] chore: 음성 켜고 끄기 행 이름을 마이크 고정으로 변경
    마이크 송출과 나란히 놓였을 때 무엇이 다른지가 이름에서 읽힌다. 하나는
    누르고 있는 동안, 하나는 눌러서 고정이다.

--- Assets/_Game/Client/Settings/SettingsStyle.cs
+++ Assets/_Game/Client/Settings/SettingsStyle.cs
@@ -432,7 +432,7 @@ namespace Game.Client.Settings
                     case Core.Settings.ControlAction.MicrophoneTalk:
                         return "마이크 송출";
                     case Core.Settings.ControlAction.VoiceToggle:
-                        return "음성 켜고 끄기";
+                        return "마이크 고정";
                     case Core.Settings.ControlAction.MoveForward:
                         return "앞으로 이동";
                     case Core.Settings.ControlAction.MoveLeft:
